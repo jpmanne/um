796f1cad50adabdc509d057014d95a6eb3477069
Cloud User <centos@ip-172-31-51-134.ec2.internal>
2017-04-25 18:49:13 +0000

    Welcome message updated

--- src/main/webapp/js/controllers/welcomeController.js
+++ src/main/webapp/js/controllers/welcomeController.js
@@ -46,5 +46,5 @@ function welcomeController($scope, $http, $cookies, $location, $timeout, urlServ
 
 		logoutService.userLogout(res, urlValue);
 	}
-	$scope.welcomeMsg = "Welcome ";
+	$scope.welcomeMsg = "Welcome to the demo";
 }
